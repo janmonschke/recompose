f21e9ad8b8ef89df6e36a2867e9967d933e9be56
Jan Monschke <jan.monschke@gmail.com>
2016-06-27 17:28:13 +0200

    make sure to pass an array to shouldUpdate

--- src/packages/recompose/onlyUpdateForKeys.js
+++ src/packages/recompose/onlyUpdateForKeys.js
@@ -3,12 +3,14 @@ import shallowEqual from './shallowEqual'
 import createHelper from './createHelper'
 import pick from './utils/pick'
 
-const onlyUpdateForKeys = propKeys =>
-  shouldUpdate(
+const onlyUpdateForKeys = propKeys => {
+  propKeys = Array.isArray(propKeys) ? propKeys : [propKeys]
+  return shouldUpdate(
     (props, nextProps) => !shallowEqual(
       pick(nextProps, propKeys),
       pick(props, propKeys)
     )
   )
+}
 
 export default createHelper(onlyUpdateForKeys, 'onlyUpdateForKeys')
